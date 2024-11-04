91efda04e04e37eec959823436a914697dbf78fa
cocktailpeanut <cocktailpeanuts@proton.me>
2024-11-03 20:35:04 -0500

    update

--- install.js
+++ install.js
@@ -28,7 +28,8 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "pip install -r requirements.txt"
+          "pip install -r requirements.txt",
+          "pip install https://github.com/oobabooga/flash-attention/releases/download/v2.6.3/flash_attn-2.6.3+cu122torch2.3.1cxx11abiFALSE-cp310-cp310-win_amd64.whl"
         ]
       }
     },
