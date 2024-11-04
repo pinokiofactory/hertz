a96f835eb424d9d34ec8136c50a8ef46157d2a31
cocktailpeanut <cocktailpeanuts@proton.me>
2024-11-03 22:40:35 -0500

    update

--- install.js
+++ install.js
@@ -9,18 +9,6 @@ module.exports = {
         ]
       }
     },
-    // Delete this step if your project does not use torch
-    {
-      method: "script.start",
-      params: {
-        uri: "torch.js",
-        params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          // xformers: true   // uncomment this line if your project requires xformers
-        }
-      }
-    },
     // Edit this step with your custom install commands
     {
       method: "shell.run",
@@ -31,6 +19,18 @@ module.exports = {
           "pip install -r requirements.txt",
           "pip install https://github.com/oobabooga/flash-attention/releases/download/v2.6.3/flash_attn-2.6.3+cu122torch2.3.1cxx11abiFALSE-cp310-cp310-win_amd64.whl"
         ]
+      }
+    },
+    // Delete this step if your project does not use torch
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",                // Edit this to customize the venv folder path
+          path: "app",                // Edit this to customize the path to start the shell from
+          // xformers: true   // uncomment this line if your project requires xformers
+        }
       }
     },
 //    {
