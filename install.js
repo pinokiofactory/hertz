5b348d7000ca2aa95bdd60e69d132680d7bb5b66
cocktailpeanut <cocktailpeanuts@proton.me>
2024-11-04 18:47:44 -0500

    update

--- install.js
+++ install.js
@@ -28,7 +28,8 @@ module.exports = {
         message: [
           "pip install -r requirements.txt",
           "pip install wheel",
-          "pip install flash-attn --no-build-isolation"
+          //"pip install flash-attn --no-build-isolation"
+          "pip install flash-attn",
 //          "pip install https://github.com/oobabooga/flash-attention/releases/download/v2.6.3/flash_attn-2.6.3+cu122torch2.4.1cxx11abiFALSE-cp310-cp310-win_amd64.whl"
         ]
       }
