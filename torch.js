3bbd4f78e6325c74cfcd7a3b0963dbe864bc7a83
cocktailpeanut <cocktailpeanuts@proton.me>
2024-11-04 19:20:51 -0500

    update

--- torch.js
+++ torch.js
@@ -7,7 +7,7 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "pip install torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 --index-url https://download.pytorch.org/whl/cu121"
+        "message": "pip install torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 --index-url https://download.pytorch.org/whl/cu124"
       }
     },
     // windows amd
